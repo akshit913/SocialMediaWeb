3f9bc150a3c11d4b8f8ed79d1b470fa2bc567af0
akshit913 <akshit.9@gmail.com>
2020-08-25 01:51:34 -0400

    Create anonymous user, footer

--- src/public/app/social-common.js
+++ src/public/app/social-common.js
@@ -1,3 +1,20 @@
 $(() => {
     $('#navbar').load('./components/navbar.html')
+    $('#footer').load('./components/footer.html')
+    loginIfNeeded()
 })
+
+function loginIfNeeded () {
+    let currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null
+    if(!currentUser) {
+        $.post('/api/users', {},(user) => {
+            if(user) {
+                console.log('registered as ', user.username)
+                window.localStorage.user = JSON.stringify(user)
+            }
+        })
+    }else {
+        console.log('resuming session as ',currentUser.username)
+    }
+    console.log(currentUser)
+}
